72710df3774f9c5df8e23bb5a812b1d77142f748
BigSuj <sujenk@gmail.com>
2024-04-16 21:47:02 -0700

    add attention

--- prepare_data.py
+++ prepare_data.py
@@ -5,6 +5,8 @@ from collections import Counter
 from torchtext.vocab import vocab
 from torchtext.data.utils import get_tokenizer
 
+def char_tokenizer(sentence):
+    return list(sentence)
 
 def load_data(path):
     english_sentences = []
@@ -13,28 +15,26 @@ def load_data(path):
         for line in file:
             en_text, te_text = line.strip().split('++++$++++')
             english_sentences.append(en_text)
-            telugu_sentences.append(te_text)
+            telugu_sentences.append(en_text)
     return english_sentences, telugu_sentences
 
-def build_vocab(sentences, tokenizer):
+def build_vocab(sentences):
     counter = Counter()
     for sentence in sentences:
-        counter.update(tokenizer(sentence))
+        counter.update(char_tokenizer(sentence))
     return vocab(counter, specials=['<unk>', '<pad>', '<bos>', '<eos>'])
 
 
 
-def tokenize(text, vocab, tokenizer):
-    return [vocab['<bos>']] + [vocab[token] for token in tokenizer(text)] + [vocab['<eos>']]
+def tokenize(text, vocab):
+    return [vocab['<bos>']] + [vocab[char] for char in char_tokenizer(text)] + [vocab['<eos>']]
 
 class TranslationDataset(Dataset):
-    def __init__(self, src_sentences, trg_sentences, src_vocab, trg_vocab, src_tokenizer, trg_tokenizer):
+    def __init__(self, src_sentences, trg_sentences, src_vocab, trg_vocab):
         self.src_sentences = src_sentences
         self.trg_sentences = trg_sentences
         self.src_vocab = src_vocab
         self.trg_vocab = trg_vocab
-        self.src_tokenizer = src_tokenizer
-        self.trg_tokenizer = trg_tokenizer
 
     def __len__(self):
         return len(self.src_sentences)
@@ -42,10 +42,9 @@ class TranslationDataset(Dataset):
     def __getitem__(self, idx):
         src_sentence = self.src_sentences[idx]
         trg_sentence = self.trg_sentences[idx]
-        src_numericalized = tokenize(src_sentence, self.src_vocab, self.src_tokenizer)
-        trg_numericalized = tokenize(trg_sentence, self.trg_vocab, self.trg_tokenizer)
+        src_numericalized = tokenize(src_sentence, self.src_vocab)
+        trg_numericalized = tokenize(trg_sentence, self.trg_vocab)
         return torch.tensor(src_numericalized), torch.tensor(trg_numericalized)
-
 
 def collate_fn(batch):
     src_batch, trg_batch = [], []
@@ -56,16 +55,11 @@ def collate_fn(batch):
     trg_batch = pad_sequence(trg_batch, padding_value=te_vocab['<pad>']).transpose(0,1)
     return src_batch, trg_batch
 
-
-
-en_tokenizer = get_tokenizer('spacy', language='en_core_web_sm')
-te_tokenizer = get_tokenizer('basic_english')
 eng_sentences, telugu_sentences = load_data('english_telugu_data.txt')
-eng_vocab = build_vocab(eng_sentences, en_tokenizer)
-te_vocab = build_vocab(telugu_sentences, te_tokenizer)
+eng_vocab = build_vocab(eng_sentences)
+te_vocab = build_vocab(telugu_sentences)
 
 if __name__ == '__main__':
-    dataset = TranslationDataset(eng_sentences, telugu_sentences, eng_vocab, te_vocab, en_tokenizer, te_tokenizer)
-
+    dataset = TranslationDataset(eng_sentences, telugu_sentences, eng_vocab, te_vocab)
     BATCH_SIZE = 64
     dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
--- train.py
+++ train.py
@@ -1,15 +1,15 @@
-
-from models import EncoderRNN, DecoderRNN
+from models import EncoderRNN, DecoderRNN, AttnDecoderRNN
 from prepare_data import *
 from torch.utils.data import DataLoader
 import torch
+import tqdm
 
 
 
 
-dataset = TranslationDataset(eng_sentences, telugu_sentences, eng_vocab, te_vocab, en_tokenizer, te_tokenizer)
+dataset = TranslationDataset(eng_sentences, telugu_sentences, eng_vocab, te_vocab)
 
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
 
 
@@ -17,27 +17,46 @@ dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn
 eng_size = len(eng_vocab)
 te_size = len(te_vocab)
 
-learning_rate = 0.0001
-hidden_size = 65
+learning_rate = 0.001
+hidden_size = 128
 
 encoder = EncoderRNN(eng_size, hidden_size)
-decoder = DecoderRNN(hidden_size, te_size, te_vocab)
+decoder = AttnDecoderRNN(hidden_size, te_size, te_vocab)
 encoder_optim = torch.optim.Adam(encoder.parameters(), lr=learning_rate)
 decoder_optim = torch.optim.Adam(decoder.parameters(), lr=learning_rate)
 
 
-criterion = torch.nn.NLLLoss()
+criterion = torch.nn.NLLLoss(ignore_index=eng_vocab['<pad>'])
+
+def eval_sent(sentence):
+    inp = tokenize(sentence, eng_vocab)
+    tensor_inp = torch.tensor(inp).unsqueeze(0)
+    encoder.eval()
+    decoder.eval()
+
+    with torch.no_grad():
+        enc_out,enc_hidden = encoder(tensor_inp)
+        decoder_out, decoder_hidden,_ = decoder(enc_out, enc_hidden)
+
+    max_indices = torch.argmax(decoder_out, dim=-1)
+
+    l = []
+    for i in max_indices[0]:
+        l.append(te_vocab.lookup_token(i.item()))
+
+    print(''.join(l))
+    return l
 
 print(len(dataloader))
-for src,trg in dataloader:
+for src,trg in tqdm.tqdm(dataloader):
 
     encoder_optim.zero_grad()
     decoder_optim.zero_grad()
     max_length = trg.shape[-1]
     output, hidden_state = encoder(src)
-    decoder_out, decoder_hidden = decoder(output, hidden_state, max_length=max_length)
+    decoder_out, decoder_hidden,_ = decoder(output, hidden_state, trg, max_length=max_length)
 
-    decode_out = decoder_out.view(-1, decoder_out.shape[-1]) # Reshape to [batch_size * seq_len, vocab_size]
+    decode_out = decoder_out.reshape(-1, decoder_out.shape[-1]) # Reshape to [batch_size * seq_len, vocab_size]
     trg = trg.reshape(-1)  # Flatten to [batch_size * seq_len]
     loss = criterion(decode_out, trg)
     loss.backward()
@@ -46,17 +65,5 @@ for src,trg in dataloader:
     print(f'loss: {loss.item()}')
 
 
-sentence = 'What is your name?'
-inp = tokenize(sentence, eng_vocab, en_tokenizer)
-tensor_inp = torch.tensor(inp).unsqueeze(0)
-
-with torch.no_grad():
-    enc_out,enc_hidden = encoder(tensor_inp)
-    decoder_out, decoder_hidden = decoder(enc_out, enc_hidden)
-
-max_indices = torch.argmax(decoder_out, dim=-1)
-
-for i in max_indices[0]:
-    te_vocab.lookup_token(i.item())
 
 
